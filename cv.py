f49ce93c805f6dc09ac4aa38747a1d0906285fb5
Brent Baumgartner <brent@brentbaum.com>
2014-10-20 15:58:46 -0400

    Check aspect ratio to find invalid tables, tidy things up.

--- cv.py
+++ cv.py
@@ -1,32 +1,44 @@
 from __future__ import division
 from SimpleCV import *
 from cluster import HierarchicalClustering
+
 def getTableConvexHull(img):
     blue = img.colorDistance(Color.BLUE)
     s = img - blue
     blobs = s.findBlobs()
     table = blobs[-1]
-    return table.mConvexHull
+    hull = table.mConvexHull
+    return sorted([p for p in zip(hull, hull[1:] + [hull[-1]])],
+                  reverse=True, key = length)
+    
     
 def length (segment):
     return ((segment[0][0] - segment[1][0])**2 + (segment[0][1] - segment[1][1])**2)**(1/2)
 
+def asTuple(pointList):
+    return tuple(map(tuple, pointList))
+
+def getIntersection((x1, y1), (x2, y2), (x3, y3), (x4, y4), width, height):
+    if([x1, y1] != [x3, y3] and [x2, y2] != [x4, y4] and x1 != x2 and x4 != x3):
+        m1 = (y2 - y1) / (x2 - x1)    
+        m2 = (y4 - y3) / (x4 - x3)    
+        
+        b1 = y1 - m1*x1
+        b2 = y3 - m2*x3
+        
+        if m1 != m2:
+            xi = int((b2 - b1) / (m1 - m2))
+            yi = int(m1 * xi + b1)
+            if(0 <= xi and 0 <= yi and xi < width and yi < height):
+                return [xi, yi]
+                        
 def getIntersections(segments, width, height):
     intersections = []
-    for [[x1, y1], [x2, y2]] in segments[:8]:
-        for [[x3, y3], [x4, y4]] in segments[:8]:
-            if([x1, y1] != [x3, y3] and [x2, y2] != [x4, y4] and x1 != x2 and x4 != x3):
-                m1 = (y2 - y1) / (x2 - x1)    
-                m2 = (y4 - y3) / (x4 - x3)    
-
-                b1 = y1 - m1*x1
-                b2 = y3 - m2*x3
-
-                if m1 != m2:
-                    xi = int((b2 - b1) / (m1 - m2))
-                    yi = int(m1 * xi + b1)
-                    if(0 <= xi and 0 <= yi and xi < width and yi < height):
-                        intersections.append([xi, yi])
+    for [p1, p2] in segments[:8]:
+        for [p3, p4] in segments[:8]:
+            i = getIntersection(p1, p2, p3, p4, width, height)
+            if(i):
+                intersections.append(i)
     return intersections
 
 
@@ -66,12 +78,24 @@ def drawPoints(points, img):
 def toTableCoords(points):
     return map(lambda p: [p[0]-points[1][0], p[1]-points[0][1]], points)
 
-def asTuple(pointList):
-    return tuple(map(tuple, pointList))
+def validCorners(corners):
+    tCorners = toTableCoords(corners)
+    minC = min(sum(tCorners, []))
+    if(minC < 0):
+        print "Error! Invalid coordinate."
+        print corners, tCorners
+        return False
+    #lrRatio = (tCorners[0][0] - tCorners[1][0]) / (tCorners[2][0] - tCorners[3][0]) 
+    #tbRatio = (tCorners[0][1] - tCorners[2][1]) / (tCorners[1][1] - tCorners[3][1]) 
+    aspectRatio = corners[3][0]/corners[3][1]
+    if abs(aspectRatio - 1.8) > .4:
+        print "Error! Bad aspect ratio."
+        print corners, tCorners
+        return False
+    return True
 
 def cropAndPerspectiveTransform(corners, img):
     tCorners = toTableCoords(corners)
-    print tCorners
     maxX = tCorners[3][0]
     maxY = max(tCorners[1][1], tCorners[3][1])
     minC = min(sum(tCorners, []))
@@ -92,9 +116,7 @@ def makeTransformed(index):
     img = Image("img/table" + str(index) + ".png")
     hImage = img * 0 
     
-    hull = getTableConvexHull(img)
-    segments = [p for p in zip(hull, hull[1:] + [hull[-1]])]
-    segments = sorted(segments, reverse=True, key = length)
+    segments = getTableConvexHull(img)
     
     intersections = getIntersections(segments, img.width, img.height)
     
@@ -105,9 +127,11 @@ def makeTransformed(index):
     #drawPoints(intersections[:10], hImage)
     
     table = cropAndPerspectiveTransform(corners, img)
-    if(table):
+    if(table and validCorners(corners)):
         table.save("out/cropped" + str(index) + ".png")
         hImage.save("out/hull" + str(index) + ".png")
+
+    print "\n-------------\n"
 
 for x in range(0, 10):
     makeTransformed(x)
