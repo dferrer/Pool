9e5bd0e0f04f74f4353fa6154f49885c5a07aef7
Brent Baumgartner <brent@brentbaum.com>
2014-10-19 23:21:34 -0400

    Automate!

--- cv.py
+++ cv.py
@@ -11,10 +11,10 @@ def getTableConvexHull(img):
 def length (segment):
     return ((segment[0][0] - segment[1][0])**2 + (segment[0][1] - segment[1][1])**2)**(1/2)
 
-def getIntersections(segments):
+def getIntersections(segments, width, height):
     intersections = []
-    for [[x1, y1], [x2, y2]] in segments[:6]:
-        for [[x3, y3], [x4, y4]] in segments[:6]:
+    for [[x1, y1], [x2, y2]] in segments[:8]:
+        for [[x3, y3], [x4, y4]] in segments[:8]:
             if([x1, y1] != [x3, y3] and [x2, y2] != [x4, y4] and x1 != x2 and x4 != x3):
                 m1 = (y2 - y1) / (x2 - x1)    
                 m2 = (y4 - y3) / (x4 - x3)    
@@ -25,7 +25,7 @@ def getIntersections(segments):
                 if m1 != m2:
                     xi = int((b2 - b1) / (m1 - m2))
                     yi = int(m1 * xi + b1)
-                    if(0 <= xi and 0 <= yi and xi < hImage.width and yi < hImage.height):
+                    if(0 <= xi and 0 <= yi and xi < width and yi < height):
                         intersections.append([xi, yi])
     return intersections
 
@@ -46,16 +46,21 @@ def getCorners(intersections):
     cornerClusters = sorted(clusters, key=len, reverse=True)[:4]
 
     corners = map(averageCoords, cornerClusters)
-    return sorted(corners, key=lambda p: p[0] + p[1])
+    corners = sorted(corners, key= lambda p: p[0])
+    left = sorted(corners[:2], key=lambda p: p[1])
+    right = sorted(corners[2:], key=lambda p: p[1])
+    #{'top-left': left[0], 'bottom-left': left[1],
+    # 'top-right': right[0], 'bottom-right': right[1]}
+    return left[0], left[1], right[0], right[1]
 
-def drawLines(segments):
+def drawLines(segments, img):
     for segment in segments:
-        l = Line(hImage, (segment[0], segment[1]))
+        l = Line(img, (segment[0], segment[1]))
         l.draw(width=3)
 
-def drawPoints(points):
+def drawPoints(points, img):
     for point in points:
-        l = Circle(hImage, point[0], point[1], 3)
+        l = Circle(img, point[0], point[1], 3)
         l.draw(color=(255, 0, 0), width=3)
 
 def toTableCoords(points):
@@ -65,35 +70,44 @@ def asTuple(pointList):
     return tuple(map(tuple, pointList))
 
 def cropAndPerspectiveTransform(corners, img):
-    table = img.crop(corners[1][0], corners[0][1],
-                     corners[3][0]-corners[1][0],
-                     corners[3][1] - corners[0][1])
-    corners = toTableCoords(corners)
-    maxX = corners[3][0]
-    maxY = max(corners[1][1], corners[3][1])
-    dst = ((0, 0), (0, maxY), (maxX, 0), (maxX, maxY))
-    print dst, asTuple(corners)
-    result = cv.CreateMat(3, 3, cv.CV_32FC1)
-    cv.GetPerspectiveTransform(asTuple(corners), dst, result)
-    return table.transformPerspective(result)
+    tCorners = toTableCoords(corners)
+    print tCorners
+    maxX = tCorners[3][0]
+    maxY = max(tCorners[1][1], tCorners[3][1])
+    minC = min(sum(tCorners, []))
+    if(minC < 0):
+        print "Error! Invalid coordinate :("
+    else:
+        table = img.crop(corners[1][0], corners[0][1],
+                         corners[3][0]-corners[1][0],
+                         corners[3][1] - corners[0][1])
+        dst = ((0, 0), (0, maxY), (maxX, 0), (maxX, maxY))
+        result = cv.CreateMat(3, 3, cv.CV_32FC1)
+        cv.GetPerspectiveTransform(asTuple(tCorners), dst, result)
+        return table.transformPerspective(result)
 
-img = Image('table.png')
-hImage = img * 0 
+def makeTransformed(index):
+    print "Processing table " + str(index) + "."
+    
+    img = Image("img/table" + str(index) + ".png")
+    hImage = img * 0 
+    
+    hull = getTableConvexHull(img)
+    segments = [p for p in zip(hull, hull[1:] + [hull[-1]])]
+    segments = sorted(segments, reverse=True, key = length)
+    
+    intersections = getIntersections(segments, img.width, img.height)
+    
+    corners = getCorners(intersections)
+    
+    drawLines(segments[:10], hImage)
+    drawPoints(corners, hImage)
+    #drawPoints(intersections[:10], hImage)
+    
+    table = cropAndPerspectiveTransform(corners, img)
+    if(table):
+        table.save("out/cropped" + str(index) + ".png")
+        hImage.save("out/hull" + str(index) + ".png")
 
-hull = getTableConvexHull(img)
-segments = [p for p in zip(hull, hull[1:] + [hull[-1]])]
-segments = sorted(segments, reverse=True, key = length)
-
-intersections = getIntersections(segments)
-
-corners = getCorners(intersections)
-
-table = cropAndPerspectiveTransform(corners, img)
-
-corners = toTableCoords(corners)
-table.save("cropped.png")
-
-drawLines(segments)
-drawPoints(corners)
-
-hImage.save("hull.png")
+for x in range(0, 10):
+    makeTransformed(x)
